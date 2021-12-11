4df9f02c90fe9c6cc27fee5f1df6bd5b783d0db2
Techiee Guy <92099128+techieeguy@users.noreply.github.com>
2021-12-11 15:35:31 +0530

    Add files via upload

---
+++ URL Shortner with GUI in Python/url_shortner.py
@@ -0,0 +1,32 @@
+from tkinter import *       #pip install tkinter
+import pyshorteners         #pip install pyshorteners
+
+root = Tk()
+root.title('Link Shortener')
+root.geometry("500x500")
+
+def shorten():
+	if shorty.get():
+		shorty.delete(0, END)
+
+	if my_entry.get():
+		url = pyshorteners.Shortener().tinyurl.short(my_entry.get())
+		shorty.insert(END, url)
+		print(pyshorteners.Shortener().tinyurl.expand(url))
+
+my_label = Label(root, text="Enter Link To Shorten", font=("Helvetica", 24))
+my_label.pack(pady=20)
+
+my_entry = Entry(root, font=("Helvetica", 24))
+my_entry.pack(pady=20)
+
+my_button = Button(root, text="Short link", command=shorten, font=("Helvetica", 14))
+my_button.pack(pady=20)
+
+shorty_label = Label(root, text="Shortened Link", font=("Helvetica", 14))
+shorty_label.pack(pady=50)
+
+shorty = Entry(root, font=("Helvetica", 22), justify=CENTER, width=30, bd=0, bg="systembuttonface")
+shorty.pack(pady=10)
+
+root.mainloop()
